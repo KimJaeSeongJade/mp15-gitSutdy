69629ecd6e946ad7210aabb6d9278a7ec82195be
Jade <jade16work@gmail.com>
2026-08-27 11:14:10 +0900

    [Feat] 몬스터 UI 연동기능 추가

---
+++ ConsoleApp1/UI.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1;
+
+public class UI
+{
+    private Monster _target;
+    
+    public UI(Monster monster)
+    {
+        _target = monster;
+        monster.Health.Subscribe(RefreshUI);
+    }
+
+    private void RefreshUI(int value)
+    {
+        Console.WriteLine($"UI 갱신 : {value}");
+    }
+}
