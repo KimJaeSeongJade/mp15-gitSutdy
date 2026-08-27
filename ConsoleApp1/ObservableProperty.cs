ac541fd741b5da029d6a8438c7904a6640877fa4
Jade <jade16work@gmail.com>
2026-08-27 11:11:20 +0900

    [Feat] Observable Property

---
+++ ConsoleApp1/ObservableProperty.cs
@@ -0,0 +1,31 @@
+﻿
+public class ObservableProperty<T>
+{
+    private Action<T> _onValueChanged;
+    private T _value;
+
+    public T Value
+    {
+        get => _value;
+        set
+        {
+            _value = value;
+            _onValueChanged?.Invoke(value);
+        }
+    }
+    
+    public void Subscribe(Action<T> onValueChanged)
+    {
+        _onValueChanged += onValueChanged;
+    }
+
+    public void Unsubscribe(Action<T> onValueChanged)
+    {
+        _onValueChanged -= onValueChanged;
+    }
+
+    public void UnsubscribeAll()
+    {
+        _onValueChanged = null;
+    }
+}
